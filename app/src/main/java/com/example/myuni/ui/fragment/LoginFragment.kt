7f0e54a03024e6a89077eab353feb885cf27ed15
= <617172142@qq.com>
2021-07-21 21:19:36 +0100

    coding test demo to test firebase real-time communication function

--- app/src/main/java/com/example/myuni/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/myuni/ui/fragment/LoginFragment.kt
@@ -8,13 +8,15 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.myuni.R
+import com.example.myuni.model.Message
 import com.example.myuni.utils.HttpConnection
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+
 
 class LoginFragment : Fragment() {
     private lateinit var root: View
     private var params: HashMap<String, String> = HashMap<String, String>()
-    private var firebasedatabse =  FirebaseDatabase.getInstance()
+    private lateinit var dbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState)
 
@@ -37,8 +39,31 @@ class LoginFragment : Fragment() {
                 val httpConnection = HttpConnection()
                 val response = httpConnection.doPost("https://us-central1-my-uni-project-fc56b.cloudfunctions.net/addUser", params)
                 println("======$response")
-            }.start()
+            }
 
+//            var map1: HashMap<String, Any> = HashMap<String, Any>()
+//            val data1 = "{ \"name\" = \"Liven\" & \"age\" = \"23\"}"
+//            map1.put("1", data1)
+//            dbRef.updateChildren(map1)
+
+            dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    println("DATA CHANGE!!!!!!")
+                    val value = dataSnapshot.value as ArrayList<String>?
+
+                    for (i in 0..value!!.size-1){
+                        println("---($i)---" + value[i])
+                    }
+
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    println("NOTHING CHANGE!!!!!!")
+                }
+            })
+
+
+            println("click button")
         }
 
         return root
@@ -50,7 +75,34 @@ class LoginFragment : Fragment() {
     }
 
     fun test(){
-        var databaseFireReference = firebasedatabse.getReference("usersList")
-        println("--------$databaseFireReference-----")
+
+        var database = FirebaseDatabase.getInstance()
+        dbRef = database.getReference("conversationList").child("messagesId1")
+//        ArrayList<SessionBean> list= new ArrayList<>();
+//        SessionBean sessionBean = new SessionBean("hellow "+messageid+"!", "", "", "ake", System.currentTimeMillis() + "");
+//        SessionBean sessionBean1 = new SessionBean("hellow ake!", "", "", messageid, System.currentTimeMillis() + "");
+//        list.add(sessionBean);
+//        list.add(sessionBean1);
+//        reference.setValue(sessionBean);
+
+        val message = Message("test", Message.TYPE_RECEIVED)
+//        var map: HashMap<String, Any> = HashMap<String, Any>()
+//        map.put("1", message)
+//        dbRef.updateChildren(map)
+
+        var map: HashMap<String, Any> = HashMap<String, Any>()
+        val data = "{ \"name\" = \"jackson1\" & \"age\" = \"23\"}"
+        val data1 = "{ \"name\" = \"Liven1\" & \"age\" = \"23\"}"
+        map.put("0", data)
+        map.put("1", data1)
+        dbRef.setValue(map)
+
+        //dbRef.updateChildren(map)
+
+//        dbRef.setValue("this is text")
+
+
     }
+
+
 }
